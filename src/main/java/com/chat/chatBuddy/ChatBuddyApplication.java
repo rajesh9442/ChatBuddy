ae1e033f7b43beb885a25b261eaa2d7f639f75bc
RajeshMahendran <rajeshmahendran1999@gmail.com>
2024-04-27 11:59:39 -0500

    Started SpringBoot App

---
+++ src/main/java/com/chat/chatBuddy/ChatBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chatBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBuddyApplication.class, args);
+	}
+
+}
